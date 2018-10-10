4b9a14f85617e747a3ed90fa43df2dd291cf715e
Ben Sheldon <bensheldon@gmail.com>
2018-10-10 15:54:05 -0700

    Add webmock to test suite

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  render_views
 
+  describe '#index' do
+    it 'renders' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
