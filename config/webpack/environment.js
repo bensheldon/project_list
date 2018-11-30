bfc7f75c9b8931d880ba5c731618561d834bce28
Ben Sheldon <bensheldon@gmail.com>
2018-11-30 07:58:24 -0800

    Fix missing jQuery JS errors

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,24 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack');
 
-// environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
-//   $: 'jquery',
-//   jQuery: 'jquery',
-//   'window.jQuery': 'jquery',
-//   Popper: ['popper.js', 'default']
-// }));
-
 module.exports = environment;
+
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}));
+
+environment.loaders.append('expose', {
+  test: require.resolve('jquery'),
+  use: [
+    {
+      loader: 'expose-loader',
+      options: 'jQuery'
+    },
+    {
+      loader: 'expose-loader',
+      options: '$'
+    }
+  ]
+});
